dad156cef24455bfd952fe4370de101e1c92b726
Jeffry Jimenez <shino17jt@gmail.com>
2025-03-09 16:18:18 -0600

    reinstalled vue

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+  optimizeDeps: {
+    include: ['vue'],
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: ['vue'],
+    }
+  }
+});

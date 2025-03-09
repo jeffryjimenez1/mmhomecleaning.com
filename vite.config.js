6cc51760eb307c2ef8368d98ddcebb1c02903661
Jeffry Jimenez <shino17jt@gmail.com>
2025-03-09 16:39:48 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './', // Ensures correct paths in production
   plugins: [vue(), vueDevTools(), tailwindcss()],
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   build: {
     rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./src/main.js', import.meta.url))
-      }
+      input: fileURLToPath(new URL('./src/main.js', import.meta.url))
     }
   }
 });
-

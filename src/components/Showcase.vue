9d95ddfa7f97b708f0942e95ac9c1f707ba49ab7
Jeffry Jimenez <shino17jt@gmail.com>
2025-03-19 20:11:23 -0600

    changed picture showcase

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -2,13 +2,12 @@
 import { ref } from 'vue';
 
 // Dynamically load background image
-const bgImage = ref(new URL('@/assets/img/cleaning.jpg', import.meta.url).href);
+// const bgImage = ref(new URL('@/assets/img/cleaning.jpg', import.meta.url).href);
 </script>
 
 <template>
   <div
-      class="h-[calc(100vh-50px)] mt-[90px] flex items-center bg-blue-100 relative z-1 showcase-wrapper"
-      :style="{ backgroundImage: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${bgImage})` }">
+      class="h-[calc(100vh-50px)] mt-[90px] flex items-center bg-blue-100 relative z-1 showcase-wrapper">
     <div class="text-white md:ml-30 h-auto px-6 md:px-20 text-center md:text-left hero-text">
       <h1 class="text-4xl md:text-7xl mb-5 font-bold">MM HomeCleaning</h1>
       <p class="text-lg md:text-2xl">Reliable, Efficient, and Affordable Home Cleaning Services. Let Us Make Your Home Shine!</p>
